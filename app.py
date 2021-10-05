494c66661f179bfae962d5131c7e6dd1261a4e99
SruthyManohar <mail2sruthymanohar@gmail.com>
2021-10-05 13:41:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 
-  return '<h1><center>FlaskApplication - Version - 7</center></h1>'
+  return '<h1><center>FlaskApplication - Version - 8</center></h1>'
 
 if __name__ == '__main__':
 
